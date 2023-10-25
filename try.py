5a97295cf2fe17a29e04e60bb3802b7033d0c901
kirtisikka <kirtisikka972@gmail.com>
2023-10-25 19:00:37 +0530

    added loading screen

--- try.py
+++ try.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, Response
+from django.shortcuts import render
+from flask import Flask, render_template, Response, send_from_directory
 import cv2
 import cvzone
 import math
 import time
+from flask.templating import _render
 from ultralytics import YOLO
 import os
 from flask import Flask, redirect, render_template, request, Response
 from werkzeug.utils import secure_filename
 from flask import Flask, redirect, render_template, request, Response
 app = Flask(__name__)
+import cv2
+
 video_path = None
  # For Video
 # Define the upload folder and allowed file extensions
@@ -17,6 +21,10 @@ app.config['ALLOWED_EXTENSIONS'] = {'mp4'}
 
 # Ensure the upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+output_folder = 'output_videos'
+
+# Create the folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
 model = YOLO("../Yolo-Weights/yolov8l.pt")
 class ObjectDetector:
     def __init__(self, video_path, model_path):
@@ -41,9 +49,9 @@ class ObjectDetector:
 
         self.frame_width = int(self.cap.get(3))
         self.frame_height = int(self.cap.get(4))
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.output_video = cv2.VideoWriter('output_video.mp4', self.fourcc, 30.0, (self.frame_width, self.frame_height))
-
+        self.fourcc = cv2.VideoWriter_fourcc(*'X264')
+        self.output_video_path = os.path.join(output_folder, 'output_video.mp4')
+        self.output_video = cv2.VideoWriter(self.output_video_path, self.fourcc, 30.0, (self.frame_width, self.frame_height))
 
 
     def detect_objects(self):
@@ -85,8 +93,8 @@ class ObjectDetector:
                 cv2.putText(img, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
                 self.output_video.write(img)  # Write the frame to the output video
-                _, frame = cv2.imencode('.jpg', img)
-                yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
+                # _, frame = cv2.imencode('.jpg', img)
+                # yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -102,10 +110,13 @@ def index():
             file.save(video_path)
             video_path = video_path
             print(video_path)
+            detector = ObjectDetector(video_path, "../Yolo-Weights/yolov8l.pt")
+            detector.detect_objects()
+          
             # return Response(ObjectDetector( video_path ,"../Yolo-Weights/yolov8l.pt").detect_objects(), mimetype='multipart/x-mixed-replace; boundary=frame')
-            response = Response(ObjectDetector(video_path ,"../Yolo-Weights/yolov8l.pt").detect_objects(), mimetype='multipart/x-mixed-replace; boundary=frame')
-            response.headers['Refresh'] = '10;url=/another_page'  # Redirect to /another_page after 5 seconds
-            return response
+            # response = Response(ObjectDetector(video_path ,"../Yolo-Weights/yolov8l.pt").detect_objects(), mimetype='multipart/x-mixed-replace; boundary=frame')
+            # response.headers['Refresh'] = '5;url=another_page'  # Redirect to /another_page after 5 seconds
+            # return response
             # session['redirect_url'] = '/another_page'
             # return Response(ObjectDetector(video_path, "../Yolo-Weights/yolov8l.pt").detect_objects(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
@@ -114,9 +125,35 @@ def index():
          
 
     return render_template('upload.html')
+def get_frame():
+    folder_path = 'output_videos/output_video.mp4'
+    image_path=folder_path    
+    video = cv2.VideoCapture(image_path)  # detected video path
+    while True:
+        success, image = video.read()
+        if not success:
+            break
+        ret, jpeg = cv2.imencode('.jpg', image)   
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')   
+        time.sleep(1)  #control the frame rate to display one frame every 100 milliseconds: 
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(get_frame(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/another_page')
 def another_page():
-    return render_template('another_page.html')
+   
+    if os.path.exists('output_videos/output_video.mp4'):
+        return render_template('another_page.html')
+    else:
+        return render_template('processing.html')
+    
+@app.route('/download_video')
+def download_video():
+    return send_from_directory('output_videos','output_video.mp4', as_attachment=True)
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5009)
